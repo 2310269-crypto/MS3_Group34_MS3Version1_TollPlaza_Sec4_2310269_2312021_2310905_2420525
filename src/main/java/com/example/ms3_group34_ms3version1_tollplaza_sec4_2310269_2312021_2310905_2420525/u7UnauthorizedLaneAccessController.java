b85d1f42b26c5a5a27ac6a02379249b72164fda0
ruhtvt <ruhafarhad@gmail.com>
2026-08-09 02:09:33 +0600

    edited

--- src/main/java/com/example/ms3_group34_ms3version1_tollplaza_sec4_2310269_2312021_2310905_2420525/u7UnauthorizedLaneAccessController.java
+++ src/main/java/com/example/ms3_group34_ms3version1_tollplaza_sec4_2310269_2312021_2310905_2420525/u7UnauthorizedLaneAccessController.java
@@ -1,4 +1,10 @@
 package com.example.ms3_group34_ms3version1_tollplaza_sec4_2310269_2312021_2310905_2420525;
 
+import javafx.scene.control.TextField;
+
 public class u7UnauthorizedLaneAccessController {
+    @javafx.fxml.FXML
+    private TextField unauthorizedLaneNumberTF;
+    @javafx.fxml.FXML
+    private TextField dateOfUnauthorizedLaneTF;
 }
